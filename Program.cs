d332120ba7bf8039abc02b7c8eb993b453724936
Valdiney Daniel Garcia <valdineysc@gmail.com>
2023-02-14 00:13:07 -0300

    Primeira versão sem1_exercicio3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double nota1, nota2, nota3;
+double mediaTotal;
+Console.WriteLine("Bem vindo aluno! Informe sua primeira nota (0 - 10):");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe a segunda nota (0 - 10):");
+nota2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe a terceira nota (0 - 10):");
+nota3 = double.Parse(Console.ReadLine());
+mediaTotal = (nota1 + nota2 + nota3)/3;
+    if(mediaTotal >= 6 && mediaTotal <= 10) {
+    Console.WriteLine($"Parabéns! Você está aprovado com média {mediaTotal}");
+}
+    else if(mediaTotal >= 5 && mediaTotal < 6 ) {
+    Console.WriteLine($"Você está em recuperação com média {mediaTotal}");
+}
+    else {
+        Console.WriteLine($"Com a média {mediaTotal}, você está reprovado!");
+    }
